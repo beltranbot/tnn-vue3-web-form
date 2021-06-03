84569610b7a0f044217ac786ea91d7199ad715c3
beltranbot <beltranbot@gmail.com>
2021-06-03 09:43:33 -0500

    deleting skills

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -16,7 +16,7 @@
     <input type="text" v-model="tempSkill" @keyup="addSkill">
 
     <div v-for="skill in skills" :key="skill" class="pill">
-      {{skill}}
+      <span @click="deleteSkill(skill)">{{skill}}</span>
     </div>
 
     <div class="terms">
@@ -52,6 +52,9 @@ export default {
         }
         this.tempSkill = ''
       }
+    },
+    deleteSkill(skill) {
+      this.skills = this.skills.filter(item => item !== skill)
     }
   }
 }
@@ -90,5 +93,17 @@ export default {
     margin: 0 10px 0 0;
     position: relative;
     top: 2px;
+  }
+  .pill {
+    display: inline-block;
+    margin: 20px 10px 0 0;
+    padding: 6px 12px;
+    background: #eee;
+    border-radius: 20px;
+    font-size: 12px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    color: #777;
+    cursor: pointer;
   }
 </style>
