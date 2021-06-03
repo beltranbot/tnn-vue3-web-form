14372c4c29d3fc80fbccccdee2c0e0aeb01d1070
beltranbot <beltranbot@gmail.com>
2021-06-03 09:53:04 -0500

    added form validation

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,10 +1,13 @@
 <template>
-  <form>
+  <form @submit.prevent="handleSubmit">
     <label>Email:</label>
     <input type="email" required v-model="email">
 
     <label>Password:</label>
     <input type="password" required v-model="password">
+    <div v-if="passwordError" class="error">
+      {{passwordError}}
+    </div>
 
     <label>Role</label>
     <select v-model="role">
@@ -24,6 +27,10 @@
       <label>Accept terms and conditions</label>
     </div>
 
+    <div class="submit">
+      <button>Create an account</button>
+    </div>
+
   </form>
   <p>Email: {{email}}</p>
   <p>Password: {{password}}</p>
@@ -40,7 +47,8 @@ export default {
       role: 'developer',
       terms: false,
       tempSkill: '',
-      skills: []
+      skills: [],
+      passwordError: ""
     }
   },
   methods: {
@@ -55,6 +63,18 @@ export default {
     },
     deleteSkill(skill) {
       this.skills = this.skills.filter(item => item !== skill)
+    },
+    handleSubmit() {
+      // validate password
+      this.passwordError = this.password.length > 5 ?
+        "" : "Password must be at least 6 chars long"
+      if (!this.passwordError) {
+        console.log("email: ", this.email)
+        console.log("password: ", this.password)
+        console.log("role: ", this.role)
+        console.log("skills: ", this.skills)
+        console.log("terms accepted: ", this.terms)
+      }
     }
   }
 }
@@ -105,5 +125,22 @@ export default {
     font-weight: bold;
     color: #777;
     cursor: pointer;
+  }
+  button {
+    background: #0b6dff;
+    border: 0;
+    padding: 10px 20px;
+    margin-top: 20px;
+    color: white;
+    border-radius: 20px;
+  }
+  .submit {
+    text-align: center;
+  }
+  .error {
+    color: #ff0062;
+    margin-top: 10px;
+    font-size: 0.8em;
+    font-weight: bold;
   }
 </style>
